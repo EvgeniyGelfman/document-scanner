bcfc7dbdfd7e13cabe2ce1508ae84bf999a2a673
Yevgen Gelfman <euginegelfman@gmail.com>
2025-09-04 19:52:28 +0300

    Move to parent folder

---
+++ documentScanner.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+widthImg = 640
+heightImg = 480
+
+cap.set(3, widthImg)
+cap.set(4, heightImg)
+cap.set(10, 130)
+
+def preProcessing(img):
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+    imgCanny = cv2.Canny(imgBlur, 200, 200)
+    kernel = np.ones((5,5))
+    imgDial = cv2.dilate(imgCanny, kernel, iterations=2)
+    imgThres = cv2.erode(imgDial, kernel, iterations=1)
+
+    return imgThres
+
+def getContours(img):
+    biggest = np.array()
+    maxArea = 0
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 5000:
+            # cv2.drawContour(imgContour, cnt, -1, (0, 255, 0), 2)
+            perimeter = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
+            if area > maxArea and len(approx) == 4:
+                biggest = approx
+                maxArea = area
+    cv2.drawContour(imgContour, biggest, -1, (0, 255, 0), 20)
+    return biggest
+
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), np.float32)
+    add = myPoints.sum(1)
+    diff = np.diff(myPoints, axis=1)
+
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+
+    return myPointsNew
+
+
+
+def getWarp(img, biggest):
+    biggest = reorder(biggest)
+    pts1 = np.float32(biggest)
+    pts = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts)
+    imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+
+    imgCropped = imgOutput[20:imgOutput.shape[0]-20, 20:imgOutput.shape[1]-20]
+    imgCropped = cv2.resize(imgCropped, (widthImg, heightImg))
+    return imgCropped
+
+while True:
+    success, img = cap.read()
+    img = cv2.resize(img, (widthImg, heightImg))
+    imgContour = img.copy()
+    imgThres = preProcessing(img)
+
+
+    cv2.imshow('Result', imgThres)
+    biggest = getContours(imgThres)
+    imgWarp = getWarp(img, biggest)
+
+    cv2.imshow('Result', imgWarp)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q') or key == 27:  # 'q' or ESC to quit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
